42208359a0123d660fd396677f396c22117d977b
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-09-20 00:09:18 +0000

    + utils for page and layout + default page and layout

--- next/index.ts
+++ next/index.ts
@@ -1,11 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { ImageResponse } from 'next/og'
 import inject from 'fnts/inject'
+import { FunctionComponent, ReactNode } from 'react'
+
+declare global {
+    // Returns NextResponse, handles the request and returns a Response
+    type NextRouteHandler<
+        P = {},
+        B = any,
+    > = (request: NextRequest, params: { params: P }) => Promise<
+        NextResponse<B> | Response | ImageResponse
+    >
+    // Doesn't need to return NextResponse, handles the request and returns a result
+    type NextRequestHandler<
+        P = {},
+        R extends any = void
+    > = (request: NextRequest, params?: { params: P }) => R
+
+    type NextLayoutHandler<P extends Record<string, string>> = FunctionComponent<P & { [SLOT: string] : ReactNode }>
+    type NextPageHandler<P extends Record<string, string>> = FunctionComponent<P>
+}
+
 
 export const handleNextRouteAsync = <
     P = {}
 >(handler: NextRouteHandler<P>): NextRouteHandler<P> => (
     (request, params) => (
+        // Wraps the handler to facilitate debugging errors, worst thing is not knowing what caused a bug (in my experience)
         handler(request, params).catch(
             inject(console.error)(
                 (error: Error) => NextResponse.json(
@@ -23,6 +44,14 @@ export const handleNextRouteAsync = <
     )
 )
 
+export const route = handleNextRouteAsync
+export const layout = <P extends Record<string, string> = Record<string, string>>( handler: NextLayoutHandler<P> ) => (
+    (props: P) => handler(props)
+)
+export const page = <P extends Record<string, string> = Record<string, string>>( handler: NextLayoutHandler<P> ) => (
+    (props: P) => handler(props)
+)
+
 export const returnRedirect = (url: string, auth?: string) => {
     return new NextResponse(null, {
         status: 302,
@@ -31,19 +60,4 @@ export const returnRedirect = (url: string, auth?: string) => {
             ...(auth ? { Authorization: auth } : {})
         }
     })
-}
-
-declare global {
-    // Returns NextResponse
-    type NextRouteHandler<
-        P = {},
-        B = any,
-    > = (request: NextRequest, params: { params: P }) => Promise<
-        NextResponse<B> | Response | ImageResponse
-    >
-    // Doesn't need to
-    type NextRequestHandler<
-        P = {},
-        R extends any = void
-    > = (request: NextRequest, params?: { params: P }) => R
 }
