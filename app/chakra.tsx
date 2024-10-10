4e8da4398c35e08efafb9e5b075384bbab38c97c
Icaro Asas Virtuais <39817766+asasvirtuais@users.noreply.github.com>
2024-10-10 11:27:57 +0000

    + Chakra, font, and colors

---
+++ app/chakra.tsx
@@ -0,0 +1,33 @@
+'use client'
+import { ChakraProvider } from '@chakra-ui/provider'
+import { extendTheme } from '@chakra-ui/react'
+import { PropsWithChildren } from 'react'
+import { Comfortaa, Major_Mono_Display } from 'next/font/google'
+
+export const title = Major_Mono_Display({weight: '400', subsets: ['latin']})
+
+export const body = Comfortaa({ subsets: ['latin'] })
+
+const theme = extendTheme({
+    fonts: {
+        body: body.style.fontFamily,
+        heading: title.style.fontFamily
+    },
+    colors: {
+        candy: {
+            yummy: '#4A3FA3',
+            tasty: '#7B6F6F',
+            smelly: '#8E8282',
+            slicky: '#8F8D8E',
+            slipery: '#A5A5A5'
+        }
+    }
+})
+
+export default function ({ children }: PropsWithChildren) {
+    return (
+        <ChakraProvider theme={theme}>
+            {children}
+        </ChakraProvider>
+    )
+}
