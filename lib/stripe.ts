2ec11d5494afce7b9ccac3e2f3ce40548a5c0da9
Icaro Asas Virtuais <asasvirtuais.dev@gmail.com>
2026-02-26 20:50:41 -0300

    more bullshit

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -4,9 +4,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
     throw new Error('Missing STRIPE_SECRET_KEY environment variable')
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-01-27.acacia',
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export async function ensureStripeCustomer({
     email,
