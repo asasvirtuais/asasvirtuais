16f12d15ec0c6cca64f3ac88d03cc79b5b054c53
Icaro Asas Virtuais <asasvirtuais.dev@gmail.com>
2026-02-26 10:16:14 -0300

    + applied authentication

--- app/interface.ts
+++ app/interface.ts
@@ -1,25 +1,31 @@
 'use server'
+import { authenticate } from 'asasvirtuais/auth0'
 import { firestoreInterface } from './firestore-interface'
 import { schema } from './schema'
 
 const rootInterface = firestoreInterface<typeof schema, keyof typeof schema>(schema)
 
 export const find = (async (props) => {
-    return rootInterface.find(props)
+    const user = (await authenticate()).id
+    return rootInterface.find({ ...props, user })
 }) as typeof rootInterface.find
 
 export const list = (async (props) => {
-    return rootInterface.list(props)
+    const user = (await authenticate()).id
+    return rootInterface.list({ ...props, user })
 }) as typeof rootInterface.list
 
 export const create = (async (props) => {
-    return rootInterface.create(props)
+    const user = (await authenticate()).id
+    return rootInterface.create({ ...props, user })
 }) as typeof rootInterface.create
 
 export const update = (async (props) => {
-    return rootInterface.update(props)
+    const user = (await authenticate()).id
+    return rootInterface.update({ ...props, user })
 }) as typeof rootInterface.update
 
 export const remove = (async (props) => {
-    return rootInterface.remove(props)
+    const user = (await authenticate()).id
+    return rootInterface.remove({ ...props, user })
 }) as typeof rootInterface.remove
